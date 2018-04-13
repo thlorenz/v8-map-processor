06fbd036ce1fea6f005ba41cca0a63f37aa04d83
Thorsten Lorenz <thlorenz@gmx.de>
2018-04-13 16:33:49 -0600

    example: adding points example

---
+++ example/points.js
@@ -0,0 +1,45 @@
+'use strict'
+
+/* global print */
+
+class Point {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  distance(p) {
+    const squareX = Math.pow(p.x - this.x, 2)
+    const squareY = Math.pow(p.y - this.y, 2)
+    return Math.sqrt(squareX + squareY)
+  }
+
+  toString() {
+    return `x: ${this.x}, y: ${this.y}`
+  }
+}
+
+for (var i = 0; i < 4; i++) {
+  for (var j = 0; j < 4; j++) {
+    const pointA = new Point(i, j)
+    const pointB = new Point(j, i)
+    print(pointA.distance(pointB))
+  }
+}
+
+const point3d = new Point(1, 2)
+point3d.z = 3
+print(point3d.toString())
+
+// allocating t before z will cause transition from 2d map
+let point4d = new Point(1, 2)
+point4d.t = 4
+point4d.z = 5
+print(point4d.toString())
+
+// allocating t after z will cause transition from 3d map
+point4d = new Point(1, 2)
+point4d.z = 4
+point4d.t = 5
+
+print(point4d.toString())
